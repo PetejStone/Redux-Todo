7a2bd06d0804537b56ce7c5192bbd01134749d87
PetejStone <petejstoney@gmail.com>
2019-05-14 13:29:29 -0700

    got text rendering on screen to test if it everything is hooked up

---
+++ todo/src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Todo extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <h1>Testing</h1>
+        
+        )}
+}
+
+export default Todo;
